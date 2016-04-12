7ef9c2c87b9f6b7ec36ecac555f68bbf694798a5
L <lrider313@gmail.com>
2016-04-12 11:23:18 +0900

    FreeBoard 및 FreeBoardReply를 위한 Model(DTO), Mapper(XML, Interface)를 생성. bean_dao.xml에 FbMapper 및 FbrpMapper 설정 추가.

---
+++ src/main/java/com/genius/model/Fbrp.java
@@ -0,0 +1,40 @@
+package com.genius.model;
+
+public class Fbrp {
+	private String fbrpcont;
+	private Integer fbrec;
+	private Integer fbrej;
+	private Integer fbid;
+	private String memid;
+	public String getFbrpcont() {
+		return fbrpcont;
+	}
+	
+	public void setFbrpcont(String fbrpcont) {
+		this.fbrpcont = fbrpcont;
+	}
+	public Integer getFbrec() {
+		return fbrec;
+	}
+	public void setFbrec(Integer fbrec) {
+		this.fbrec = fbrec;
+	}
+	public Integer getFbrej() {
+		return fbrej;
+	}
+	public void setFbrej(Integer fbrej) {
+		this.fbrej = fbrej;
+	}
+	public Integer getFbid() {
+		return fbid;
+	}
+	public void setFbid(Integer fbid) {
+		this.fbid = fbid;
+	}
+	public String getMemid() {
+		return memid;
+	}
+	public void setMemid(String memid) {
+		this.memid = memid;
+	}
+}
